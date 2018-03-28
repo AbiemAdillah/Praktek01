5a0b2283904a408ed3a2b5aea9a0c570562cda42
AbiemAdillah <Abiem.adillah@gmail.com>
2018-03-28 23:09:45 +0800

    Tambahan

---
+++ src/praktek01/Dota2.java
@@ -0,0 +1,33 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package praktek01;
+
+/**
+ *
+ * @author Windows 8.1 Pro
+ */
+public class Dota2 {
+    String nama;
+    String role;
+    String typehero;
+    String atktype;
+    String skill1;
+    String skill2;
+    String skill3;
+    String ulti;
+    
+    
+    void cetakInfo(){
+        System.out.println("Nama : "+nama);
+        System.out.println("Role : "+role);
+        System.out.println("TYpe Hero :"+typehero);
+        System.out.println("Attack Type : "+atktype);
+        System.out.println("Skill 1 : "+skill1);
+        System.out.println("Skill 2 : "+skill2);
+        System.out.println("Skill 3 : "+skill3);
+        System.out.println("Ultimate Skill : "+ulti);
+}
+}
